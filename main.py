96cd6eccfda33716eff9d506de22527986e74bcf
Kevin Tsai <ktsai@imsmetals.com>
2026-05-08 11:16:45 -0700

    Update main logic; add streamlit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import holidays
 import numpy as np
+import streamlit as st
 from datetime import datetime
 
 
@@ -42,7 +44,7 @@ def is_workday(date: np.datetime64) -> bool:
 
     return False
 
-def count_workdays(start_date: np.datetime64, end_date: np.datetime64) -> int:
+def count_workdays(start_date: np.datetime64, end_date: np.datetime64, holidays: list=[]) -> int:
     '''Count number of workdays between two dates.
 
     Args:
@@ -52,4 +54,44 @@ def count_workdays(start_date: np.datetime64, end_date: np.datetime64) -> int:
     Returns:
         Number of workdays (Mon-Fri)
     '''
-    return int(np.busday_count(start_date, end_date))
+    return int(np.busday_count(begindates=start_date, enddates=end_date, holidays=holidays))
+
+
+user_year = input('>>Enter year: ')
+start_date = standardize_date(f'{user_year}-01-01')
+end_date = standardize_date(f'{user_year}-12-31')
+
+HOLIDAY_EXCLUSIONS = [
+    'Birthday of Martin Luther King, Jr.',
+    "Washington's Birthday",
+    'Veterans Day',
+    'Juneteenth National Independence Day',
+    'Columbus Day',
+    'Christmas Eve'
+]
+
+us_fed_holidays = holidays.US(years=user_year, categories=holidays.GOVERNMENT)
+
+# Remove excluded holidays (wildcard match)
+filtered_holidays = {}
+for date, name in us_fed_holidays.items():
+    if not any(word in name for word in HOLIDAY_EXCLUSIONS):
+        filtered_holidays[date] = name
+
+holiday_dates = []
+for date in filtered_holidays.keys():
+    holiday_dates.append(standardize_date(str(date)))
+
+num_workdays = count_workdays(start_date, end_date, holiday_dates)
+
+
+
+'''
+Year: 2026
+Workdays: 123
+
+Holidays
+#######
+
+
+'''
